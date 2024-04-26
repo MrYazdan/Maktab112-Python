8d7616f23d2c56efffe10f492b60231241c1518d
MrYazdan <mrrezayazdani@yahoo.com>
2024-04-26 12:21:10 +0330

    :zap: S14 -> OOP Concept 2

---
+++ 15-encapsulate-date-hiding-on_other-file.py
@@ -0,0 +1,11 @@
+# file = __import__("15-encapsulate-date-hiding")
+# print(file.maktab_sharif_account.balance)
+# print(file._reza_account.name)
+# print(file.__sina_account.name)
+# print(file.__amir_account__.name)
+
+# from t1 import public_var, _protect_var, __private_var
+#
+# print(public_var)
+# print(_protect_var)
+# print(__private_var)
